67ff2dddcac4ce088a08e34ca8f0d73984947a85
Pranav-Komirishetty <125572877+Pranav-Komirishetty@users.noreply.github.com>
2023-07-20 15:03:46 +0530

    Update coindesk.service.ts
    ..

--- src/app/api/coindesk.service.ts
+++ src/app/api/coindesk.service.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { catchError,throwError,Observable } from 'rxjs';
+
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CoindeskService {
-  url="https://api.coindesk.com/v1/bpi/currentprice.json";
-
+  url="https://api.coindesk.com/v1/bpi/currentprice.json"; //
+  errorMessage = '';
   constructor(private coinApi:HttpClient) { }
 
-  coinData(){
-    return this.coinApi.get(this.url);
+  coinData() : Observable<any>{
+    return this.coinApi.get(this.url).pipe(
+      catchError(this.handleError));
+  }
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
