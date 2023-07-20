88cd3128f74325645c5086ff4735f28e35dabf55
Pranav-Komirishetty <125572877+Pranav-Komirishetty@users.noreply.github.com>
2023-07-20 14:59:07 +0530

    Update app-routing.module.ts
    ..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,25 +3,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmployeeInfoComponent } from './employee-info/employee-info.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { HomeComponent } from './home/home.component';
-import {SampleComponent} from './sample/sample.component'
+import {SampleComponent} from './sample/sample.component';
+import {UrlErrorComponent} from './url-error/url-error.component';
+import{AddressComponent} from './address/address.component';
+import{PersonalInfoComponent} from './personal-info/personal-info.component';
 
 const routes: Routes = [
-  {
-    component:EmployeeListComponent,
-    path:'employee-list'
-  },
-  {
-    component:EmployeeInfoComponent,
-    path:'employee-info'
-  },
-  {
-    component:SampleComponent,
-    path:'api'
-  },
-  {
-    component:HomeComponent,
-    path:''
-  }
+  { path:'', redirectTo : 'home', pathMatch : 'full'},
+  { component : HomeComponent,
+    path : 'home' },
+  { component:EmployeeListComponent,
+    path:'employee-list',children:[{
+      path : '', redirectTo:'address', pathMatch:'full' },
+    { component : AddressComponent,
+      path : 'address' },
+    { component : PersonalInfoComponent,
+      path : 'personal-info'}
+    ]},
+  { component:EmployeeInfoComponent,
+    path:'employee-info'},
+  { component:SampleComponent,
+    path:'api' },
+  { component : UrlErrorComponent,
+    path:'**'}
 ];
 
 @NgModule({
